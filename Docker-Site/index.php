00cbe2d61e24af9944ac67bbe0d757898069c565
Daniel Pastoor <daniel@pastoortjes.nl>
2026-04-04 14:31:26 +0200

    Add Dockerfile and index.php for PHP Apache container setup

---
+++ Docker-Site/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hello World</title>
+</head>
+<body>
+  <h1>Hello World from container <?php echo gethostname(); ?></h1>
+</body>
+</html>
